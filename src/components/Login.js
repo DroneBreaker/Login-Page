e20dc09dfc9fd8afa62ee35e205455bbc346c25d
DroneBreaker <brownierz01@gmail.com>
2023-01-22 12:54:23 +0000

    Final finishing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
-import Register from "./Register";
 
 const Login = ({ loginIntro, onFormSwitch }) => {
-  const initialValues = { email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [error, setError] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setError(validate(formValues));
-    setIsSubmit(true);
+    setErrors(validate(formValues));
+    console.log("Submitted successfully");
   };
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+    const nextFormValueState = {
+      ...formValues,
+      [e.target.name]: e.target.value,
+    };
+
+    setFormValues(nextFormValueState);
   };
 
   useEffect(() => {
-    if (Object.keys(error).length === 0 && isSubmit) {
-      console.log(formValues);
+    if (Object.keys(error).length === 0 && formValues !== "") {
+      alert("Signed in successfully");
     }
   }, [error]);
 
@@ -49,11 +51,6 @@ const Login = ({ loginIntro, onFormSwitch }) => {
 
   return (
     <div>
-      {Object.keys(error).length === 0 && isSubmit ? (
-        <div>Signed in successfully!</div>
-      ) : (
-        <pre></pre>
-      )}
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="bg-gray-100 h-[70vh] md:h-[100vh]">
           <h1 className="font-semibold text-xl mt-10 md:mt-20 mx-10 md:mx-20">
@@ -70,7 +67,7 @@ const Login = ({ loginIntro, onFormSwitch }) => {
             <Button
               bgColor={"white"}
               img={"#"}
-              location="mail.google.com"
+              // location="mail.google.com"
               title={"Sign in with Google"}
             />
           </div>
@@ -85,39 +82,31 @@ const Login = ({ loginIntro, onFormSwitch }) => {
               Email*
             </label>
             <input
+              name="email"
               className="rounded-full text-sm w-60 md:w-64 px-4 p-2 text-gray-500"
               placeholder="mail@website.com"
               onChange={handleChange}
             />
-            <p className="text-red-500 text-sm">{error.email}</p>
+            {error.email && (
+              <p className="text-red-500 text-sm">{error.email}</p>
+            )}
 
             <label htmlFor="password" className="block mt-2">
               Password*
             </label>
             <input
+              name="password"
               type={"password"}
               className="rounded-full text-sm w-60 md:w-64 px-4 p-2 text-gray-500 mb-6"
               placeholder="Min. 8 character"
               onChange={handleChange}
             />
-            <p className="text-red-500 text-sm mb-2">{error.password}</p>
+            {error.password && (
+              <p className="text-red-500 text-sm mb-2">{error.password}</p>
+            )}
 
             <Button bgColor={"blue"} title="Login" />
           </form>
-
-          {/* <div className="flex mt-2 font-medium mx-8 md:mx-20">
-            <input
-              className="mx-1 mt-[-10px]"
-              type="checkbox"
-              value="Remember me"
-            />
-
-            <label className="text-xs">Remember me</label>
-
-            <button className="mt-[-6px] mx-5 md:mx-14 mb-4">
-              <a className="text-violet-500 text-xs">Forget password?</a>
-            </button>
-          </div> */}
 
           <div className="mt-4">
             <p className="mx-10 md:mx-20 text-xs font-semibold">
@@ -133,7 +122,26 @@ const Login = ({ loginIntro, onFormSwitch }) => {
         </div>
 
         <div className="bg-gradient-to-l from-red-500 to-blue-700">
-          <div className="h-64 w-64 rounded-full bg-gray-500 mx-6 md:mx-24 mt-6 mb-2 md:mt-24 animate-spin"></div>
+          <div className="h-64 w-64 rounded-full bg-gray-300 mx-6 md:mx-24 mt-6 mb-2 md:mt-24 animate-pulse">
+            <div
+              style={{ left: "20%" }}
+              className="bg-white h-10 w-10 rounded-full relative"
+            >
+              <img src="#" />
+            </div>
+
+            <div
+              style={{ left: "90%", top: "20%" }}
+              className="bg-white h-10 w-10 rounded-full relative animate-spin"
+            >
+              <img src="#" />
+            </div>
+
+            <div
+              style={{ left: "35%", top: "60%" }}
+              className="bg-white h-10 w-10 rounded-full relative"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,25 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 
 const Register = ({ onFormSwitch }) => {
-  const [fullname, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const initialValues = { fullname: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [error, setError] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setError(validate(formValues));
-    setIsSubmit(true);
+    console.log("Registered successfully!");
   };
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+    const nextFormValueState = {
+      ...formValues,
+      [e.target.name]: e.target.value,
+    };
+
+    setFormValues(nextFormValueState);
   };
+
+  useEffect(() => {
+    if (Object.keys(error).length === 0 && formValues !== "") {
+      alert("Registered successfully!");
+    }
+  }, [error]);
 
   const validate = (values) => {
     const errors = {};
@@ -28,7 +34,7 @@ const Register = ({ onFormSwitch }) => {
 
     if (!values.fullname) {
       errors.fullname = "Full name is required";
-    } else if (!values.fullname.length < 4) {
+    } else if (values.fullname.length < 4) {
       errors.fullname = "Full name shouldn't be less than 4 characters";
     }
 
@@ -48,11 +54,6 @@ const Register = ({ onFormSwitch }) => {
 
   return (
     <div>
-      {Object.keys(error).length === 0 && isSubmit ? (
-        <div className="text-green-500 text-sm">Registered successfully!</div>
-      ) : (
-        <pre></pre>
-      )}
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="bg-gray-100 h-[70vh] md:h-[100vh]">
           <h1 className="font-semibold text-xl mt-10 md:mt-20 mx-10 md:mx-20">
@@ -82,49 +83,44 @@ const Register = ({ onFormSwitch }) => {
               Full Name*
             </label>
             <input
+              name="fullname"
               className="rounded-full text-sm w-60 md:w-64 px-4 p-2 text-gray-500"
               placeholder="Richard Browne"
               onChange={handleChange}
             />
-            <p className="text-red-500 text-sm">{error.fullname}</p>
+            {error.fullname && (
+              <p className="text-red-500 text-sm">{error.fullname}</p>
+            )}
 
             <label htmlFor="email" className="block">
               Email*
             </label>
             <input
+              name="email"
               className="rounded-full text-sm w-60 md:w-64 px-4 p-2 text-gray-500"
               placeholder="mail@website.com"
               onChange={handleChange}
             />
-            <p className="text-red-500 text-sm">{error.email}</p>
+            {error.email && (
+              <p className="text-red-500 text-sm">{error.email}</p>
+            )}
 
             <label htmlFor="password" className="block mt-2">
               Password*
             </label>
             <input
+              name="password"
               type={"password"}
               className="rounded-full text-sm w-60 md:w-64 px-4 p-2 text-gray-500 mb-6"
               placeholder="Min. 8 character"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleChange}
             />
-            <p className="text-red-500 text-sm mb-2">{error.password}</p>
+            {error.password && (
+              <p className="text-red-500 text-sm mb-2">{error.password}</p>
+            )}
 
-            <Button bgColor={"blue"} location={"/"} title="Register" />
+            <Button bgColor={"blue"} title="Register" />
           </form>
-
-          {/* <div className="flex mt-2 font-medium mx-8 md:mx-20">
-            <input
-              className="mx-1 mt-[-10px]"
-              type="checkbox"
-              value="Remember me"
-            />
-
-            <label className="text-xs">Remember me</label>
-
-            <button className="mt-[-6px] mx-5 md:mx-14 mb-4">
-              <a className="text-violet-500 text-xs">Forget password?</a>
-            </button>
-          </div> */}
 
           <div className="mt-4">
             <p className="mx-10 md:mx-20 text-xs font-semibold">
@@ -139,8 +135,8 @@ const Register = ({ onFormSwitch }) => {
           </div>
         </div>
 
-        <div className="bg-blue-600">
-          <div className="h-64 w-64 rounded-full bg-gray-500 mx-6 md:mx-24 mt-6 mb-2 md:mt-24 animate-spin"></div>
+        <div className="bg-gradient-to-l from-blue-700 to-red-500">
+          <div className="h-64 w-64 rounded-full bg-gray-300 mx-6 md:mx-24 mt-6 mb-2 md:mt-24 animate-pulse"></div>
         </div>
       </div>
     </div>
